0a1391426fbb4c2f673e0e20b44019f4c06b2320
Shruthi <shruthimarri97@gmail.com>
2021-03-30 17:47:40 +0530

    [Shruthi] : Flip Coin percentage of head and tail implemented

--- BasicProgramDemo/Program.cs
+++ BasicProgramDemo/Program.cs
@@ -4,9 +4,14 @@ namespace BasicProgramDemo
 {
     class Program
     {
+        /// <summary>
+        /// Entry point for the console application
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            ReplaceString.ReadInput();
+            FlipCoin flip = new FlipCoin();
+            flip.ReadInput();
             Console.Read();
         }
     }
