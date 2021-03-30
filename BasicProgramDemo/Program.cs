8c1d32ff315642c48238407db3ad1ed4c7d36720
Shruthi <shruthimarri97@gmail.com>
2021-03-30 09:36:48 +0530

    [Shruthi] : Replacing a string implemented

---
+++ BasicProgramDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicProgramDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReplaceString.ReadInput();
+            Console.Read();
+        }
+    }
+}
---
+++ BasicProgramDemo/ReplaceString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramDemo
+{
+    class ReplaceString
+    {
+        public static void ReadInput()
+        {
+            string template = "Hello <<UserName>>, How are you?";
+            Console.WriteLine("please enter your name");
+            string userInput = Console.ReadLine();
+            ReplaceWord(template, userInput);
+        }
+        private static void ReplaceWord(string template, string userInput)
+        {
+            string result = template.Replace("<<UserName>>", userInput);
+            Console.WriteLine("After done replace " + result);
+        }
+    }
+}
