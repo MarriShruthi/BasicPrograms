68b990fc9083e2fe1bc758672bce0d9b9659795c
Shruthi <shruthimarri97@gmail.com>
2021-03-30 19:38:03 +0530

    [Shruthi] : Power of two table implemented

---
+++ BasicProgramDemo/PowerOfTwo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgramDemo
+{
+    class PowerOfTwo
+    {
+        public void ReadInput()
+        {
+            Console.WriteLine("Enter a number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Power(n);
+        }
+        public static void Power(int n)
+        {
+            for(int i=0; i<=n; i++)
+            {
+                double result = Math.Pow(2, i);
+                Console.WriteLine("2 ^ "+i +" = " + result);
+            }
+        }
+    }
+}
--- BasicProgramDemo/Program.cs
+++ BasicProgramDemo/Program.cs
@@ -3,16 +3,11 @@
 namespace BasicProgramDemo
 {
     class Program
-    {
-        /// <summary>
-        /// Entry point for the console application
-        /// </summary>
-        /// <param name="args"></param>
+    { 
         static void Main(string[] args)
         {
-            FlipCoin flip = new FlipCoin();
-            flip.ReadInput();
-            Console.Read();
+            PowerOfTwo power = new PowerOfTwo();
+            power.ReadInput();
         }
     }
 }
